62fb10bb715007e4e3af338025dbc2d08ba7edac
Yashpa12 <yashbhamare1004@gmail.com>
2024-02-01 16:19:28 +0530

    update the addtion method

--- project/8-social-meadia-bigOne/components/CreatePost.jsx
+++ project/8-social-meadia-bigOne/components/CreatePost.jsx
@@ -1,23 +1,95 @@
-const CreatePost = ()=>{
-return (
+import { useContext, useRef } from "react";
+import { PostContext } from "../store/post-list-data";
+
+const CreatePost = () => {
+  const { createpost } = useContext(PostContext);
+
+  const useridElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const postagsElement = useRef();
+  const postReactionElement = useRef();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const userid = useridElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const postags = postagsElement.current.value.split("  ");
+    const postReaction = postReactionElement.current.value;
+
+    createpost(userid, postBody, postTitle, postags, postReaction);
+    
+  };
+  return (
     <>
-    <form>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
+      <form className="create" onSubmit={handleChange}>
+        <div className="mb-3">
+          <label htmlFor="User-id" className="form-label">
+            User ID
+          </label>
+          <input
+            type="text"
+            ref={useridElement}
+            className="form-control"
+            id="user-id"
+            placeholder="Enter the user Id"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Post Name
+          </label>
+          <input
+            type="text"
+            ref={postTitleElement}
+            className="form-control"
+            id="title"
+            placeholder="Enter the post name"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="body" className="form-label">
+            Post Content
+          </label>
+          <textarea
+            type="text"
+            ref={postBodyElement}
+            className="form-control"
+            id="body"
+            rows="4"
+            placeholder="Tell us about the post"
+          ></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="reaction" className="form-label">
+            Number of reaction
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="rection"
+            ref={postReactionElement}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Tags" className="form-label">
+            Enter here Tags
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="tags"
+            ref={postagsElement}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Post
+        </button>
+      </form>
     </>
-)
-}
-export default CreatePost
+  );
+};
+export default CreatePost;
--- project/8-social-meadia-bigOne/store/post-list-data.jsx
+++ project/8-social-meadia-bigOne/store/post-list-data.jsx
@@ -11,13 +11,29 @@ const Postreducer = (currentvalue, action) => {
     newAddvalue = currentvalue.filter(
       (post) => post.id !== action.payload.postId
     );
+  }else if (action.type == 'Add_list'){
+    newAddvalue = [action.payload,...currentvalue]
   }
   return newAddvalue;
 };
 const PostlistProvider = ({ children }) => {
   const [postlist, dispatchpostlist] = useReducer(Postreducer, default_list);
 
-  const createpost = () => {};
+  const createpost = (userid,postBody,postReaction,postTitle,postags) => {
+    console.log(userid,postBody,postReaction,postTitle,postags)
+    dispatchpostlist ({
+      type : 'Add_list',
+      payload : {
+        id: Date.now(),
+    title: postTitle,
+    body: postBody,
+    reaction: postReaction,
+    userId: userid,
+    tags:postags,
+   
+      }
+    })
+  };
 
   const Deletepost = (postId) => {
     dispatchpostlist({
@@ -48,9 +64,10 @@ const default_list = [
     title: "Pass the BC",
     body: "I am very happy to clear the graduate dregee in computer sciene field . for that I was take Bca field",
     reaction: 12,
+    userId: "user-10",
     tags: ["Hapoy", "Bca", "dream"],
 
-    userId: "user-10",
+    
   },
 ];
 export default PostlistProvider;
